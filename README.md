130e1eb3cbcd5ce6b2eca000ff11784682cba67a
kwokster10 <kwokster10@hotmail.com>
2015-05-11 14:48:46 -0400

    single athlete page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sports_sinatra
+# Athletes of the World
+
+Sinatra practice: a persistant CRUD app with atheletes as models
+
+![Michael Jordan](http://www.eonline.com/eol_images/Entire_Site/2014513/rs_634x1141-140613051823-634.Michael-Jordan-JR-61314.jpg)
--- server.rb
+++ server.rb
@@ -20,3 +20,10 @@ get '/athletes' do
 	athletes_list = db.execute("select * from athletes")
 	erb :athletes, locals: { athletes: athletes_list }
 end
+
+get '/athletes/:id' do
+	id = params[:id].to_i
+	one_athlete = db.execute("select * from athletes where id = ?;", id)
+	erb :show, locals: {athlete: one_athlete[0]}
+end
+
